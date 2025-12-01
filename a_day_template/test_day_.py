8250d383e793e3d7802a5c4b8103fa86e2e1adf3
Gnonpi <legnonpi@gmail.com>
2025-12-01 19:48:27 +0100

    readding 2023 day template

---
+++ a_day_template/test_day_.py
@@ -0,0 +1,34 @@
+from xxx.advent_interaction import get_problem_input
+from xxx.solution_day_ import (
+    CURRENT_DAY,
+    solve_part_1,
+    solve_part_2,
+    parse_problem,
+)
+
+SAMPLE = """
+"""
+
+def test_parse_problem():
+    assert parse_problem(SAMPLE) ==
+
+class TestPart1:
+    def test_solve_sample(self):
+        expected = None
+        assert solve_part_1(SAMPLE) == expected
+
+    def test_solve_real_problem(self):
+        input_string = get_problem_input(CURRENT_DAY)
+        accepted_value = None
+        assert solve_part_1(input_string) == accepted_value
+
+
+class TestPart2:
+    def test_solve_sample(self):
+        expected = None
+        assert solve_part_2(SAMPLE) == expected
+
+    def test_solve_real_problem(self):
+        input_string = get_problem_input(CURRENT_DAY)
+        accepted_value = None
+        assert solve_part_2(input_string) == accepted_value
